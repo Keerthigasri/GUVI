327bec14107a58d4fac39c6944f2562593c6dbee
Keerthigasri <46148844+Keerthigasri@users.noreply.github.com>
2019-05-31 12:16:25 +0530

    Create alph.py

---
+++ alph.py
@@ -0,0 +1,6 @@
+ch=input()
+l=['a','e','i','o','u','A','E','I','O','U''b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
+if ch in l:
+  print("Alphabet")
+else:
+  print("No")
